7a3f51456bd3085ac91ab5f655821cd727fb2754
Sourav Das <souravdas4816@gmail.com>
2023-06-02 13:04:17 +0530

    Add files via upload

---
+++ js/style-switcher.js
@@ -0,0 +1,30 @@
+
+/*------------------- theme colors -------------------  */
+const alternateStyle = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color) {
+    alternateStyle.forEach((style) =>{
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled", "true")
+        }
+    })
+}
+
+/*------------------- dark mode -------------------  */
+const dayNight = document.querySelector(".day-night");
+
+dayNight.addEventListener("click", () =>{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    document.body.classList.toggle("dark");
+})
+
+window.addEventListener("load", ()=>{
+    if(document.body.classList.contains("dark")){
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");    }
+})
