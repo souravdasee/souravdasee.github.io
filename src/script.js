7531ecb0687c94d2fbed3078cbda18455bfe2d6d
Sourav Das <souravdas4816@gmail.com>
2023-11-27 22:31:29 +0530

    Added footer section and some UI changes

--- src/script.js
+++ src/script.js
@@ -30,7 +30,4 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
 
 const headerLogoConatiner = document.querySelector('.header__logo-container');
 
-headerLogoConatiner.addEventListener(
-  'click',
-  () => (location.href = 'index.html')
-);
+headerLogoConatiner.addEventListener('click', () => (location.href = './'));
